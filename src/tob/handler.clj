8fac8e9e1646bd3ab22c0b805410e9ce8c63ce7b
Jethro Kuan <jethrokuan95@gmail.com>
2016-01-22 15:13:35 +0800

    Working slack invite w/o db

--- src/tob/handler.clj
+++ src/tob/handler.clj
@@ -3,18 +3,26 @@
             [yada.yada :refer [yada] :as yada]
             [yada.resource :refer [resource]]
             [schema.core :as s]
+            [tob.slack :as slack]
             [tob.html :as html]))
 
 (def signup-resource
   (yada (resource
-         {:description "Signup Page"    
-          :methods {:get {:parameters {:query {(s/optional-key :channel) String}}
+         {:description "Slack Invites"
+          :methods {:get {:parameters {:query {(s/required-key :channel) String}}
                           :produces [{:media-type #{"text/html"}
                                       :charset "UTF-8"}]
                           :response (fn [ctx]
                                       (let [c (get-in ctx [:parameters :query :channel])]
                                         (case (yada/content-type ctx)
-                                          "text/html" (html/signup-page c))))}}})))
+                                          "text/html" (html/signup-page c))))}
+                    :post {:parameters {:form {:channel String :email String}}
+                           :consumes [{:media-type #{"application/x-www-form-urlencoded"}
+                                       :charset "UTF-8"}]
+                           :response (fn [ctx]
+                                       (let [email (get-in ctx [:parameters :form :email])
+                                             channel (get-in ctx [:parameters :form :channel])]
+                                         "text/html" (slack/process-signup email channel)))}}})))
 
 (def fof-handler
   (yada "404 not found."))
--- src/tob/html.clj
+++ src/tob/html.clj
@@ -2,6 +2,10 @@
   (:require [hiccup.core :refer :all]
             [hiccup.page :refer :all]))
 
+;;;;;;;;;;;;
+;; Layout ;;
+;;;;;;;;;;;;
+
 (defn layout [& content]
   (html
    [:head
@@ -9,6 +13,32 @@
     [:link {:href "https://fonts.googleapis.com/css?family=Lato|Dosis:700" :type "text/css" :rel "stylesheet"}]
     (include-css "/resources/css/main.css")]
    [:body content]))
+
+;;;;;;;;;;;;
+;; Errors ;;
+;;;;;;;;;;;;
+(defn- error-page [text]
+  (layout
+   [:div.container
+    [:div.hero
+     [:div#logo
+      [:img {:src "/resources/img/logo-cry.png"}]]
+     [:div#hero-text
+      [:h1 "Oops!"]
+      [:p text]
+      [:button {:onClick "javascript:history.back()"} "Go Back"]]]]))
+
+(def error-already-invited-page (error-page "You're already invited. Please check your email again."))
+
+(def error-already-in-team-page (error-page "You're already in Slack. You can sign in <a href=\"http://tk.sg/slack\">here</a> with your credentials."))
+
+(def error-403-page (error-page "Error 403. Permission Denied."))
+
+(def error-404-page (error-page "Error 404. Page not found."))
+
+;;;;;;;;;;;;;;;;;;
+;; Slack Signup ;;
+;;;;;;;;;;;;;;;;;;
 
 (defn signup-page [chn]
   (layout
@@ -24,3 +54,13 @@
        [:button {:class "btn-large" :type "submit"}
         "Get your Slack Invite!"]]]]]))
 
+(def signup-success-page
+  (layout
+   [:body
+    [:div.container
+     [:div.hero
+      [:div#logo
+       [:img {:src "/resources/img/logo-with-text.png"}]]
+      [:div#hero-text.flow-text
+       [:h1 "Success!"]
+       [:p "Check your email to verify your invite."]]]]]))
