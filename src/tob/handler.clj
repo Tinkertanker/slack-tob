2c777c4e34c242eba0760c9f163ef8b10c5e1b2c
Jethro Kuan <jethrokuan95@gmail.com>
2016-01-22 15:52:58 +0800

    Refactored resources into own file

--- src/tob/handler.clj
+++ src/tob/handler.clj
@@ -1,28 +1,6 @@
 (ns tob.handler
   (:require [bidi.ring :refer [make-handler resources-maybe]]
-            [yada.yada :refer [yada] :as yada]
-            [yada.resource :refer [resource]]
-            [schema.core :as s]
-            [tob.slack :as slack]
-            [tob.html :as html]))
-
-(def signup-resource
-  (yada (resource
-         {:description "Slack Invites"
-          :methods {:get {:parameters {:query {(s/required-key :channel) String}}
-                          :produces [{:media-type #{"text/html"}
-                                      :charset "UTF-8"}]
-                          :response (fn [ctx]
-                                      (let [c (get-in ctx [:parameters :query :channel])]
-                                        (case (yada/content-type ctx)
-                                          "text/html" (html/signup-page c))))}
-                    :post {:parameters {:form {:channel String :email String}}
-                           :consumes [{:media-type #{"application/x-www-form-urlencoded"}
-                                       :charset "UTF-8"}]
-                           :response (fn [ctx]
-                                       (let [email (get-in ctx [:parameters :form :email])
-                                             channel (get-in ctx [:parameters :form :channel])]
-                                         "text/html" (slack/process-signup email channel)))}}})))
+            [yada.yada :refer [yada] :as yada]))
 
 (def fof-handler
   (yada "404 not found."))
@@ -31,7 +9,7 @@
   (yada "admin stuff"))
 
 (def routes
-  ["/" {"" signup-resource
+  ["/" {"" (yada signup-resource)
         "admin/" {"" admin-handler}
         "resources/" (resources-maybe {:prefix "public/"})
         true fof-handler}])
